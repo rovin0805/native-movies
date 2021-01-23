fbe0d17c28084181946db98e1e6ca6c4c2e9fabb
rovin0805 <qhdrn67@naver.com>
2021-01-23 17:05:09 +0900

    api call test

--- api.js
+++ api.js
@@ -10,20 +10,33 @@ const makeRequest = (path, params) =>
     },
   });
 
+const getAnything = async (path, params = {}) => {
+  try {
+    const {
+      data: { results },
+      data,
+    } = await makeRequest(path, params);
+    return [results || data, null];
+  } catch (error) {
+    console.log(error);
+    return [null, error];
+  }
+};
+
 export const movieApi = {
-  nowPlaying: () => makeRequest("/movie/now_playing"),
-  popular: () => makeRequest("/movie/popular"),
-  upcoming: () => makeRequest("/movie/upcoming", { region: "kr" }),
-  search: (query) => makeRequest("/search/movie", { query }),
-  movie: (id) => makeRequest(`/movie/${id}`),
-  discover: () => makeRequest("/discover/movie"),
+  nowPlaying: () => getAnything("/movie/now_playing"),
+  popular: () => getAnything("/movie/popular"),
+  upcoming: () => getAnything("/movie/upcoming", { region: "kr" }),
+  search: (query) => getAnything("/search/movie", { query }),
+  movie: (id) => getAnything(`/movie/${id}`),
+  discover: () => getAnything("/discover/movie"),
 };
 
 export const tvApi = {
-  today: () => makeRequest("/tv/airing_today"),
-  thisWeek: () => makeRequest("/tv/on_the_air"),
-  topRated: () => makeRequest("/tv/top_rated"),
-  popular: () => makeRequest("/tv/popular"),
-  search: (query) => makeRequest("/search/tv", { query }),
-  show: (id) => makeRequest(`/tv/${id}`),
+  today: () => getAnything("/tv/airing_today"),
+  thisWeek: () => getAnything("/tv/on_the_air"),
+  topRated: () => getAnything("/tv/top_rated"),
+  popular: () => getAnything("/tv/popular"),
+  search: (query) => getAnything("/search/tv", { query }),
+  show: (id) => getAnything(`/tv/${id}`),
 };
