87a15a80d7b242ecd5650228d818538f38377135
rovin0805 <qhdrn67@naver.com>
2021-01-24 21:23:41 +0900

    added styles Detail page

--- screens/Detail.js
+++ screens/Detail.js
@@ -1,10 +1,65 @@
 import React, { useEffect } from "react";
-import { View, Text } from "react-native";
+import { Dimensions } from "react-native";
+import styled from "styled-components/native";
+import { apiImage } from "../api";
+import Poster from "../components/Poster";
+import ScrollContainer from "../components/ScrollContainer";
+import Votes from "../components/Votes";
+
+const Header = styled.View`
+  height: ${Dimensions.get("window").height / 3}px;
+  align-items: center;
+  justify-content: flex-end;
+`;
+
+const BG = styled.Image`
+  width: 100%;
+  height: 100%;
+  opacity: 0.4;
+  position: absolute;
+`;
+
+const Container = styled.View`
+  flex-direction: row;
+  align-items: center;
+  top: 30px;
+`;
+
+const Info = styled.View`
+  width: 50%;
+  margin-left: 40px;
+`;
+
+const Title = styled.Text`
+  color: white;
+  font-weight: 600;
+  font-size: 24px;
+  margin-bottom: 10px;
+`;
+
+const Data = styled.View`
+  margin-top: 70px;
+  padding: 0px 30px;
+`;
+
+const DataName = styled.Text`
+  color: white;
+  opacity: 0.8;
+  font-size: 16px;
+  font-weight: 800;
+  margin-bottom: 5px;
+`;
+
+const DataValue = styled.Text`
+  color: white;
+  opacity: 0.8;
+  font-weight: 500;
+`;
 
 export default ({
   navigation,
   route: {
-    params: { id, title },
+    params: { id, title, backgroundImage, poster, votes, overview },
   },
 }) => {
   useEffect(() => {
@@ -14,8 +69,25 @@ export default ({
   }, []);
 
   return (
-    <View>
-      <Text>{id} </Text>
-    </View>
+    <ScrollContainer loading={false}>
+      <Header>
+        <BG source={{ uri: apiImage(backgroundImage, ".") }} />
+        <Container>
+          <Poster url={poster} />
+          <Info>
+            <Title>{title}</Title>
+            {votes && <Votes votes={votes} />}
+          </Info>
+        </Container>
+      </Header>
+      <Data>
+        {overview && (
+          <>
+            <DataName>Overview</DataName>
+            <DataValue>{overview}</DataValue>
+          </>
+        )}
+      </Data>
+    </ScrollContainer>
   );
 };
