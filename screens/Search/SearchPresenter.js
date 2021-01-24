802993531177f43f6a48a58394a9f1d8208c9e85
rovin0805 <qhdrn67@naver.com>
2021-01-24 19:19:04 +0900

    worked Search Screen

---
+++ screens/Search/SearchPresenter.js
@@ -0,0 +1,46 @@
+import React from "react";
+import styled from "styled-components/native";
+import Input from "../../components/Search/Input";
+import HorizontalSlider from "../../components/HorizontalSlider";
+import Vertical from "../../components/Vertical";
+
+const Container = styled.ScrollView`
+  background-color: black;
+`;
+
+export default ({ movies, shows, keyword, onChange, onSubmit }) => (
+  <Container>
+    <Input
+      placeholder={"🔎 Write a Keyword"}
+      value={keyword}
+      onChange={onChange}
+      onSubmit={onSubmit}
+    />
+    {movies?.length > 0 && (
+      <HorizontalSlider title={"Movie Results"}>
+        {movies.map((movie) => (
+          <Vertical
+            key={movie.id}
+            id={movie.id}
+            votes={movie.vote_average}
+            title={movie.title}
+            poster={movie.poster_path || movie.backdrop_path}
+          />
+        ))}
+      </HorizontalSlider>
+    )}
+    {shows?.length > 0 && (
+      <HorizontalSlider title={"TV Results"}>
+        {shows.map((show) => (
+          <Vertical
+            key={show.id}
+            id={show.id}
+            votes={show.vote_average}
+            title={show.name}
+            poster={show.poster_path}
+          />
+        ))}
+      </HorizontalSlider>
+    )}
+  </Container>
+);
